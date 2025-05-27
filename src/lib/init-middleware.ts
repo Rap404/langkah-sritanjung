ed8112cd1e52d7f971c9c042b8c8b63ca7a576bb
Rap404 <adibrafli37@gmail.com>
2025-05-27 23:33:57 +0700

    fix: init-middleware;

--- src/lib/init-middleware.ts
+++ src/lib/init-middleware.ts
@@ -1,12 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Middleware = (req: NextApiRequest, res: NextApiResponse, next: (err?: any) => void) => void;
+type Middleware = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: (err?: unknown) => void
+) => void;
 
 export function initMiddleware(middleware: Middleware) {
   return (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
     new Promise((resolve, reject) => {
-      middleware(req, res, (result: any) => {
-        return result instanceof Error ? reject(result) : resolve(result);
+      middleware(req, res, (result?: unknown) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve();
       });
     });
 }
