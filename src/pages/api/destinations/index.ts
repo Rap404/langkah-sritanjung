9c676ae47c5f323fc5b65df4f033aa12abe8b63a
Rap404 <adibrafli37@gmail.com>
2025-05-29 19:58:07 +0700

    fix: destination fields

--- src/pages/api/destinations/index.ts
+++ src/pages/api/destinations/index.ts
@@ -17,20 +17,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const { data, error } = await supabase
         .from("destination")
         .select('*, destinations_homestays ( homestay (*) ), destinations_transportations ( transportation (*) ), destinations_culinaries ( culinary (*) )')
-        .order('created_at', { ascending: false });
+        .order('created_at', { ascending: true });
         if (error) return res.status(500).json({ error: error.message });
         return res.status(200).json(data as Destination[]);
     }
 
     if (req.method === "POST") {
-        const { name, description, location, actions, timelist, image, category_id } = req.body;
+        const { name, location, actions, timelist, image, category_id } = req.body;
 
         const slug = slugify(name, {
             lower: true,})
 
         const { data, error } = await supabase
             .from("destination")
-            .insert([{ name, slug, location, description, actions, timelist, image, category_id }])
+            .insert([{ name, slug, location, actions, timelist, image, category_id }])
             .select()
             .single();
         if (error) return res.status(500).json({ error: error.message });
